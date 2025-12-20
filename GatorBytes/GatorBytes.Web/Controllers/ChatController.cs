47af567c266561c50f96f4981eb8afd07e262134
sholt <sholt1234@gmail.com>
2025-12-20 16:03:06 -0500

    a simple possibility

--- GatorBytes/GatorBytes.Web/Controllers/ChatController.cs
+++ GatorBytes/GatorBytes.Web/Controllers/ChatController.cs
@@ -24,45 +24,22 @@ public class ChatController : ControllerBase
     readonly ConfigurationValues _configValues;
     readonly IChatCompletionService _chatCompletionService;
     readonly Kernel _kernel;
-    //readonly UploadManagerBase<ResumeFileSystemUploadEntry, Resume> _uploadResumeManager;
-    //readonly UploadManagerBase<JobPostingFileSystemUploadEntry, JobPosting> _uploadJobPostingManager;
-    //readonly PromptManager _promptManager;
-    //readonly HRContext _context;
-    //readonly IMatchAnalysisManager _matchAnalysisManager;
-    //readonly IVectorProcessor _vectorProcessor;
 
 
     public ChatController(ConfigurationValues configValues,
         Kernel kernel,
-        IChatCompletionService chatCompletionService//,
-        //UploadManagerBase<ResumeFileSystemUploadEntry, Resume> uploadResumeManager,
-        //UploadManagerBase<JobPostingFileSystemUploadEntry, JobPosting> uploadJobPostingManager,
-        //HRContext context,
-        //IVectorProcessor vectorProcessor,
-        //IMatchAnalysisManager matchAnalysisManager
+        IChatCompletionService chatCompletionService
         )
     {
         _configValues = configValues;
         _chatCompletionService = chatCompletionService;
         _kernel = kernel;
-        //_uploadResumeManager = uploadResumeManager;
-        //_uploadJobPostingManager = uploadJobPostingManager;
-        //_uploadResumeManager = uploadResumeManager;
-        //_context = context;
-        //_promptManager = new PromptManager(_context);
-        //_vectorProcessor = vectorProcessor;
-        //_matchAnalysisManager = matchAnalysisManager;
+
     }
 
     [Route("/ws")]
     public async Task Get()
     {
-        //await _uploadResumeManager.ProcessEntries();
-        //await _context.SaveChangesAsync();
-        //await _uploadJobPostingManager.ProcessEntries();
-        //await _context.SaveChangesAsync();
-        //await _matchAnalysisManager.MakeMatches();
-
         var stop = 1;
 
 
@@ -93,74 +70,59 @@ public class ChatController : ControllerBase
         {
             receiveResult = await inputWebSocket.ReceiveAsync(
                 new ArraySegment<byte>(buffer), CancellationToken.None);
+
+
+            // Enable planning
+            OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+            {
+                FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(),
+            };
+
+            ChatHistory chatHistory = new ChatHistory();
+
+            chatHistory.AddSystemMessage(Prompts.TempSystemPrompt);
+            chatHistory.AddDeveloperMessage(Prompts.ResultAsRichHTMLDivRoot);
+            
+
+            while (!receiveResult.CloseStatus.HasValue)
+            {
+                var bytes = new ArraySegment<byte>(buffer, 0, receiveResult.Count);
+                var userMessage = Encoding.UTF8.GetString(bytes);
+
+                chatHistory.AddUserMessage(userMessage);
+
+
+                ChatMessageContent content = new ChatMessageContent();
+
+                var result = await chatCompletionService.GetChatMessageContentAsync(chatHistory,
+                    executionSettings: openAIPromptExecutionSettings,
+                    kernel: kernel);
+
+                result.Content = result.Content.Replace("```html", "").Replace("```", "");
+
+                var resultString = result.AsJson();
+                var resultMsgBytes = Encoding.UTF8.GetBytes(resultString);
+
+
+                await inputWebSocket.SendAsync(
+                    resultMsgBytes,
+                    receiveResult.MessageType,
+                    receiveResult.EndOfMessage,
+                    CancellationToken.None);
+
+                receiveResult = await inputWebSocket.ReceiveAsync(
+                        new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+
+            await inputWebSocket.CloseAsync(
+                receiveResult.CloseStatus.Value,
+                receiveResult.CloseStatusDescription,
+                CancellationToken.None);
+
         }
         catch (Exception ex)
         {
 
         }
-
-        // Enable planning
-        OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
-        {
-            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(),
-        };
-
-        ChatHistory chatHistory = new ChatHistory();
-        //chatHistory.AddSystemMessage(promptManager.GetSystemPrompt());
-        //chatHistory.AddDeveloperMessage(Prompts.SaveAsRichHTMLThenPDF);
-        chatHistory.AddSystemMessage(Prompts.TempSystemPrompt);
-        chatHistory.AddDeveloperMessage(Prompts.ResultAsRichHTMLDivRoot);
-
-        var isAgentChat = false;
-        var hasAgentQuestion = false;
-
-
-        var agentTest = false;
-
-        while (!receiveResult.CloseStatus.HasValue)
-        {
-            var bytes = new ArraySegment<byte>(buffer, 0, receiveResult.Count);
-            var userMessage = Encoding.UTF8.GetString(bytes);
-
-            if (isAgentChat && !hasAgentQuestion)
-            {
-                hasAgentQuestion = true;
-            }
-
-
-            chatHistory.AddUserMessage(userMessage);
-
-
-            ChatMessageContent content = new ChatMessageContent();
-
-            var result = await chatCompletionService.GetChatMessageContentAsync(chatHistory,
-                executionSettings: openAIPromptExecutionSettings,
-                kernel: kernel);
-
-            var resultString = result.AsJson();
-            var resultMsgBytes = Encoding.UTF8.GetBytes(resultString);
-
-
-            await inputWebSocket.SendAsync(
-                resultMsgBytes,
-                receiveResult.MessageType,
-                receiveResult.EndOfMessage,
-                CancellationToken.None);
-
-            try
-            {
-                receiveResult = await inputWebSocket.ReceiveAsync(
-                    new ArraySegment<byte>(buffer), CancellationToken.None);
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
-
-        await inputWebSocket.CloseAsync(
-            receiveResult.CloseStatus.Value,
-            receiveResult.CloseStatusDescription,
-            CancellationToken.None);
     }
 }
